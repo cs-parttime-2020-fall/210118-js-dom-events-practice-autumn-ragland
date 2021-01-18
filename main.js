1cb875a0c5bebaced365c2ea842d486352b198cd
autumn-ragland <raglanda998@gmail.com>
2021-01-18 14:04:04 -0600

    solution video

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 console.log(`21 01 18 JS DOM Events Practice`);
+
+// select form elements
+let title = document.querySelector("#title");
+// console.log(title);
+let author = document.querySelector("#author");
+// console.log(author);
+let submitBtn = document.querySelector("button");
+// console.log(submitBtn);
+// select book list
+let bookList = document.querySelector("#bookList");
+// console.log(bookList);
+
+// function to handel event submission
+submitForm = (event) => {
+    event.preventDefault();
+    bookList.innerHTML = bookList.innerHTML + `<div>Title : ${title.value} - Author : ${author.value}`
+
+    title.value = "";
+    author.value = "";
+}
+
+submitBtn.addEventListener("click", submitForm);
